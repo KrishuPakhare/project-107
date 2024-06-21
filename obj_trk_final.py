e3f1d07df38a47e93f4d420e56b09f880b0b2504
KrishuPakhare <165278195+KrishuPakhare@users.noreply.github.com>
2024-06-21 15:51:08 +0530

    Add files via upload

--- obj_trk_final.py
+++ obj_trk_final.py
@@ -9,47 +9,28 @@ xs = []
 ys = []
 
 video = cv2.VideoCapture("footvolleyball.mp4")
-
-# Load tracker 
+#load tracker 
 tracker = cv2.TrackerCSRT_create()
 
-# Read the first frame of the video
-returned, img = video.read()
+#read the first frame of the video
+success,img = video.read()
 
-# Select the bounding box on the image
-bbox = cv2.selectROI("Tracking", img, False)
+#selct the bounding box on the image
+bbox = cv2.selectROI("tracking",img,False)
 
-# Initialise the tracker on the img and the bounding box
-tracker.init(img, bbox)
+#initialise the tracker on the img and the bounding box
+tracker.init(img,bbox)
 
-print(bbox)
-
-def drawBox(img, bbox):
-    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
-
-    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
-
-    cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
-
-
-def goal_track(img, bbox):
-    
+def goal_track(img,bbox):
     x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
-
-    # Get the CENTER Points of the Bounding Box
     c1 = x + int(w/2)
     c2 = y + int(h/2)
-
-    # Draw a small circle using CENTER POINTS
     cv2.circle(img,(c1,c2),2,(0,0,255),5)
 
     cv2.circle(img,(int(p1),int(p2)),2,(0,255,0),3)
-
-    # Calculate Distance
     dist = math.sqrt(((c1-p1)**2) + (c2-p2)**2)
     print(dist)
 
-    # Goal is reached if distance is less than 20 pixel points
     if(dist<=20):
         cv2.putText(img,"Goal",(300,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
 
@@ -59,31 +40,31 @@ def goal_track(img, bbox):
     for i in range(len(xs)-1):
         cv2.circle(img,(xs[i],ys[i]),2,(0,0,255),5)
 
+def drawBox(img,bbox):
+    x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
+    cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
+
 while True:
-    
-    check, img = video.read()   
+   #Write the code inside loop here
+   check,img = video.read()
+   success,bbox = tracker.update(img)
+   if success :
+       drawBox(img,bbox)
+   else  :
+       cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),2) 
+   goal_track(img,bbox) 
+   cv2.imshow("results",img)
 
-    # Update the tracker on the img and the bounding box
-    success, bbox = tracker.update(img)
-
-    # Call drawBox()
-    if success:
-        drawBox(img, bbox)
-    else:
-        cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-
-    # Call goal_track()
-    goal_track(img, bbox)
-
-    # Display Video
-    cv2.imshow("result", img)
+   key = cv2.waitKey(1)
+   if key == ord('q') :
+       print("Closing")
+       break
+      
+       
 
 
-    # Quit Display Window when Spacebar key is pressed        
-    key = cv2.waitKey(25)
-    if key == 32:
-        print("Stopped")
-        break
 
+   pass
 video.release()
-cv2.destroyALLwindows()
+cv2.destroyALLwindows() 
